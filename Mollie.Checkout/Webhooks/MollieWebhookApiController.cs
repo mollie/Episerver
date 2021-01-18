7f48ee0f092c881b6f09b9ce645ba14f0ecb4fa3
Yoerie Bruijn <yoerie@arlanet.com>
2021-01-18 14:25:07 +0100

    WIP: Installed WebApi. Setup API Controller.

---
+++ Mollie.Checkout/Webhooks/MollieWebhookApiController.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Mollie.Checkout.Webhooks
+{
+    [RoutePrefix("api/molliewebhook")]
+    public class MollieWebhookApiController : ApiController
+    {
+        [HttpGet]
+        [Route("isonline")]
+        public string IsOnline()
+        {
+            return "Online!";
+        }
+
+        [HttpPost]
+        [Route("")]
+        public async Task<string> Index()
+        {
+            var json = Request.Content.ReadAsStringAsync().Result;
+
+            return "[accepted]";
+        }
+    }
+}
