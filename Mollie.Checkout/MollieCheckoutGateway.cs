5103b5414550bedcca9fc86ea73343d40534f47a
Mark Prins <mark@arlanet.com>
2021-01-29 15:35:55 +0100

    Remove outcommented code

--- Mollie.Checkout/MollieCheckoutGateway.cs
+++ Mollie.Checkout/MollieCheckoutGateway.cs
@@ -150,18 +150,5 @@ namespace Mollie.Checkout
         {
             throw new NotImplementedException("Capture not implemented yet");
         }
-
-        //private void AddNoteToOrder(IOrderGroup orderGroup, string title, string detail, Guid customerId)
-        //{
-        //    var note = orderGroup.CreateOrderNote();
-
-        //    note.Type = OrderNoteTypes.System.ToString();
-        //    note.CustomerId = customerId != Guid.Empty ? customerId : PrincipalInfo.CurrentPrincipal.GetContactId();
-        //    note.Title = !string.IsNullOrEmpty(title) ? title : detail.Substring(0, Math.Min(detail.Length, 24)) + "...";
-        //    note.Detail = detail;
-        //    note.Created = DateTime.UtcNow;
-
-        //    orderGroup.Notes.Add(note);
-        //}
     }
 }
