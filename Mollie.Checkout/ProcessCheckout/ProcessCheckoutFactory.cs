4fc09bf41c8b3ea93b21d80f16286c058dbdba07
Erwin van de Beek <erwin@arlanet.com>
2021-02-05 15:03:23 +0100

    Create Mollie Order

---
+++ Mollie.Checkout/ProcessCheckout/ProcessCheckoutFactory.cs
@@ -0,0 +1,22 @@
+﻿using EPiServer.ServiceLocation;
+using Mollie.Checkout.ProcessCheckout.Interfaces;
+using Mollie.Checkout.Services;
+
+namespace Mollie.Checkout.ProcessCheckout
+{
+    public static class ProcessCheckoutFactory
+    {
+        public static IProcessCheckout GetInstance(string languageId)
+        {
+            var checkoutConfigurationLoader = ServiceLocator.Current.GetInstance<ICheckoutConfigurationLoader>();
+            var checkoutConfiguration = checkoutConfigurationLoader.GetConfiguration(languageId);
+
+            if (checkoutConfiguration.UseOrdersApi)
+            {
+                return new ProcessOrderCheckout();
+            }
+
+            return new ProcessPaymentCheckout();
+        }
+    }
+}
