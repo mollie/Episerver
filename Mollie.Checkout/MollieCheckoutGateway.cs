ccd5f8312b875b8fcfe7831c883999e4effb22e3
Erwin van de Beek <erwin@arlanet.com>
2021-02-11 09:49:57 +0100

    ProcessPaymentCheckout in MollieCheckoutGateway niet meer nodig

--- Mollie.Checkout/MollieCheckoutGateway.cs
+++ Mollie.Checkout/MollieCheckoutGateway.cs
@@ -100,55 +100,6 @@ namespace Mollie.Checkout
             return processCheckout.Process(cart, payment);
         }
 
-        private PaymentProcessingResult ProcessPaymentCheckout(ICart cart, IPayment payment)
-        {
-            var languageId = payment.Properties[Constants.OtherPaymentFields.LanguageId] as string;
-
-            var request = _httpContextAccessor().Request;
-            var baseUrl = string.Format("{0}://{1}", request.Url.Scheme, request.Url.Authority);
-
-            var urlBuilder = new UriBuilder(baseUrl);
-
-            urlBuilder.Path = $"{Constants.Webhooks.MolliePaymentsWebhookUrl}/{languageId}";
-
-            var checkoutConfiguration = _checkoutConfigurationLoader.GetConfiguration(languageId);
-
-            var paymentClient = new Api.Client.PaymentClient(checkoutConfiguration.ApiKey);            
-
-            var paymentRequest = new Api.Models.Payment.Request.PaymentRequest
-            {
-                Amount = new Api.Models.Amount(cart.Currency.CurrencyCode, payment.Amount),
-                Description = _paymentDescriptionGenerator.GetDescription(cart, payment),
-                RedirectUrl = checkoutConfiguration.RedirectUrl + $"?orderNumber={cart.OrderNumber()}",
-                WebhookUrl = urlBuilder.ToString(),
-            };
-
-            var metaData = _checkoutMetaDataFactory.Create(cart, payment, checkoutConfiguration);
-
-            paymentRequest.SetMetadata(metaData);
-
-            var paymentResponse = paymentClient.CreatePaymentAsync(paymentRequest).Result;
-
-            if (payment.Properties.ContainsKey(Constants.OtherPaymentFields.MolliePaymentId))
-            {
-                payment.Properties[Constants.OtherPaymentFields.MolliePaymentId] = paymentResponse.Id;
-            }
-            else
-            {
-                payment.Properties.Add(Constants.OtherPaymentFields.MolliePaymentId, paymentResponse.Id);
-            }
-
-            var message = $"--Mollie Create Payment is successful. Redirect end user to {paymentResponse.Links.Checkout.Href}";
-
-            OrderNoteHelper.AddNoteToOrder(cart, "Mollie Payment created", message, PrincipalInfo.CurrentPrincipal.GetContactId());
-
-            _orderRepository.Save(cart);
-                        
-            _logger.Information(message);
-
-            return PaymentProcessingResult.CreateSuccessfulResult(message, paymentResponse.Links.Checkout.Href);
-        }
-
         private PaymentProcessingResult ProcessPaymentRefund(IOrderGroup orderGroup, IPayment payment)
         {
             throw new NotImplementedException("Refunds not implemented yet");
