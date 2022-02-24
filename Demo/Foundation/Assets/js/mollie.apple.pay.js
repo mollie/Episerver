7a16dc87564c0e4a3bb9dd46748d4fcb253e7687
Frans Boot <frans@arlanet.com>
2022-02-24 10:16:43 +0100

    moved javascript applepaysession check to seperate js file

---
+++ Demo/Foundation/Assets/js/mollie.apple.pay.js
@@ -0,0 +1,7 @@
+﻿var applePay = document.getElementById('payment-applepay');
+if (applePay) {
+    if (window.ApplePaySession && ApplePaySession.canMakePayments()) {
+        alert('apple pay active');
+        applePay.classList.remove("hidden");
+    }
+}
