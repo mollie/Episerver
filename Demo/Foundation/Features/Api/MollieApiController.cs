8bfb61f7670d6cd8a1bcfe0868bbd6be943f83e0
Frans Boot <frans@arlanet.com>
2022-03-07 13:41:56 +0100

    Working on ApplePayDirectIntegration

---
+++ Demo/Foundation/Features/Api/MollieApiController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Mollie.Checkout.MollieApi;
+
+namespace Foundation.Features.Api
+{
+    public class MollieApiController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public MollieApiController(HttpClient httpClient)
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<ActionResult> ValidateMerchant(string validationUrl)
+        {
+            var client = new MollieApplePayClient("live_jAe7MfwUsBEzqaKVyM7wVvy5aFA8md", _httpClient);
+            var response = await client.ValidateMerchant(validationUrl);
+
+            return Json(response);
+        }
+    }
+}
